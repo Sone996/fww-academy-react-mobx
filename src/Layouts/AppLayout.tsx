a8b365c8366b406ee3ce8b5549cb41c254eb5d14
nebojsa.ilic <nebojsa.ilic@factoryww.com>
2021-10-13 10:41:51 +0200

    moving navication into a component

--- src/Layouts/AppLayout.tsx
+++ src/Layouts/AppLayout.tsx
@@ -1,60 +1,11 @@
 import { observer } from "mobx-react-lite";
 import { FC } from "react";
 import { Route, Switch, useHistory } from "react-router";
-import logo from "../assets/images/factoryww.png";
-import { TOKEN_LS_NAME } from "../constants/Constants";
+import Navigation from "../components/shared/Navigation";
 import { RootStore } from "../store";
 
 const AppLayout: FC = observer(() => {
-  const { appStore, authStore } = RootStore(); // for modal state and active user
-  const history = useHistory();
-
-  //   console.log(authStore.loggedUser)
-
-  const goHome = () => {
-    if (authStore.loggedUser?.role === "teacher") {
-      history.push("/teacher-home");
-    } else {
-      history.push("/student-home");
-    }
-  };
-  const goProfile = () => {
-    // personService
-    //   .goProfile(loggedUser?.id)
-    //   .then((res) => {
-    //     dispatch({
-    //       type: ActionTypes.SET_PROFILE_DATA,
-    //       payload: res.data,
-    //     });
-    //     history.push({ pathname: `/profile/${res.data.id}` });
-    //   })
-    //   .catch((err) => {
-    //     console.log(err.response.data.errors);
-    //   });
-  };
-  const myStudents = () => {
-    history.push("/member-list");
-  };
-  const newCourseHandler = () => {
-    history.push("/new-course");
-  };
-  const CourseListHandler = () => {
-    history.push("/course-list");
-  };
-  const studentAplicationsHandler = () => {
-    history.push("/student-aplications");
-  };
-  const logout = () => {
-    authStore
-      .logoutAction()
-      .then(() => {
-        localStorage.removeItem(TOKEN_LS_NAME);
-        history.push("/login");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  const { appStore } = RootStore(); // for modal state and active user
 
   // modal logic
   //   const modalSwitch = (prop: string) => {
@@ -85,50 +36,7 @@ const AppLayout: FC = observer(() => {
       )} */}
       {/* END :: MODALS */}
       <div className="flex flex-col h-full bg-gray-500 w-2/12 items-center">
-        <div className="flex p-16">
-          <img
-            src={logo}
-            alt="logo"
-            className="cursor-pointer"
-            onClick={goHome}
-          />
-        </div>
-        <div className="flex flex-col text-white font-bold text-lg">
-          <span className="my-2 cursor-pointer" onClick={goHome}>
-            Home
-          </span>
-          <span className="my-2 cursor-pointer" onClick={goProfile}>
-            Profile
-          </span>
-          {authStore.loggedUser?.role === "teacher" ? (
-            <>
-              <span className="my-2 cursor-pointer" onClick={myStudents}>
-                My Students
-              </span>
-              <span className="my-2 cursor-pointer" onClick={newCourseHandler}>
-                {" "}
-                New Course
-              </span>
-              <span className="my-2 cursor-pointer" onClick={CourseListHandler}>
-                {" "}
-                Course List
-              </span>
-              <span
-                className="my-2 cursor-pointer"
-                onClick={studentAplicationsHandler}
-              >
-                {" "}
-                Student aplications
-              </span>
-            </>
-          ) : (
-            <></>
-          )}
-          <span className="my-2 cursor-pointer" onClick={logout}>
-            {" "}
-            Logout
-          </span>
-        </div>
+        <Navigation></Navigation>
       </div>
       <div className="flex w-full">
         {/* <Switch>
--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,7 +1,106 @@
+import { observer } from "mobx-react";
 import { FC } from "react";
+import { useHistory } from "react-router-dom";
+import { TOKEN_LS_NAME } from "../../constants/Constants";
+import { RootStore } from "../../store";
+import logo from "../../assets/images/factoryww.png";
 
-const Navigation: FC = () => {
-  return <div></div>;
-};
+const Navigation: FC = observer(() => {
+  const { authStore } = RootStore(); // for modal state and active user
+  const history = useHistory();
+
+  const goHome = () => {
+    if (authStore.loggedUser?.role === "teacher") {
+      history.push("/teacher-home");
+    } else {
+      history.push("/student-home");
+    }
+  };
+  const goProfile = () => {
+    // personService
+    //   .goProfile(loggedUser?.id)
+    //   .then((res) => {
+    //     dispatch({
+    //       type: ActionTypes.SET_PROFILE_DATA,
+    //       payload: res.data,
+    //     });
+    //     history.push({ pathname: `/profile/${res.data.id}` });
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.response.data.errors);
+    //   });
+  };
+  const myStudents = () => {
+    history.push("/member-list");
+  };
+  const newCourseHandler = () => {
+    history.push("/new-course");
+  };
+  const CourseListHandler = () => {
+    history.push("/course-list");
+  };
+  const studentAplicationsHandler = () => {
+    history.push("/student-aplications");
+  };
+  const logout = () => {
+    authStore
+      .logoutAction()
+      .then(() => {
+        localStorage.removeItem(TOKEN_LS_NAME);
+        history.push("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <div className="flex p-16">
+        <img
+          src={logo}
+          alt="logo"
+          className="cursor-pointer"
+          onClick={goHome}
+        />
+      </div>
+      <div className="flex flex-col text-white font-bold text-lg">
+        <span className="my-2 cursor-pointer" onClick={goHome}>
+          Home
+        </span>
+        <span className="my-2 cursor-pointer" onClick={goProfile}>
+          Profile
+        </span>
+        {authStore.loggedUser?.role === "teacher" ? (
+          <>
+            <span className="my-2 cursor-pointer" onClick={myStudents}>
+              My Students
+            </span>
+            <span className="my-2 cursor-pointer" onClick={newCourseHandler}>
+              {" "}
+              New Course
+            </span>
+            <span className="my-2 cursor-pointer" onClick={CourseListHandler}>
+              {" "}
+              Course List
+            </span>
+            <span
+              className="my-2 cursor-pointer"
+              onClick={studentAplicationsHandler}
+            >
+              {" "}
+              Student aplications
+            </span>
+          </>
+        ) : (
+          <></>
+        )}
+        <span className="my-2 cursor-pointer" onClick={logout}>
+          {" "}
+          Logout
+        </span>
+      </div>
+    </>
+  );
+});
 
 export default Navigation;
